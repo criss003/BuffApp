e0dc0b5abcf16b30c71110758c9073269af81ce0
Criss <>
2021-03-24 01:05:42 +0200

    ui

--- BuffSDK/BuffSDK/View/Cells/AnswerTableViewCell.swift
+++ BuffSDK/BuffSDK/View/Cells/AnswerTableViewCell.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class AnswerTableViewCell: UITableViewCell {
     @IBOutlet weak var valueLabel: UILabel!
-        
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var iconImage: UIImageView!
+    
     func configure(questionsInfoModel: QuestionsInfoModel) {
         valueLabel.text = questionsInfoModel.value
     }
     
     func selectAnswer() {
-        
+        backgroundImageView.backgroundColor = .cyan
+        iconImage.image = UIImage(named: "correct",
+                                  in: Bundle.sdkBundle,
+                                  compatibleWith: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        backgroundImageView.backgroundColor = .white
+        iconImage.image = UIImage(named: "genericAnswer",
+                                  in: Bundle.sdkBundle,
+                                  compatibleWith: nil)
     }
 }
